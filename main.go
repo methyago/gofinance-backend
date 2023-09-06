c16549d592a74b71adee162913133b2346c16a42
methyago <me0.thyago@gmail.com>
2023-09-06 11:45:15 -0300

    feat: add env

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	api "github.com/methyago/gofinance-backend/api"
 	db "github.com/methyago/gofinance-backend/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("cannot load env: ", err)
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
