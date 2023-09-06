ba796585ae9dc0dce32fbef9ef2a073a03404e66
methyago <me0.thyago@gmail.com>
2023-09-06 17:12:09 -0300

    add air and cors

--- api/server.go
+++ api/server.go
@@ -10,9 +10,26 @@ type Server struct {
 	router *gin.Engine
 }
 
+func CORSConfig() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
+
+		if context.Request.Method == "OPTIONS" {
+			context.AbortWithStatus(204)
+			return
+		}
+
+		context.Next()
+	}
+}
+
 func NewServer(store *db.SQLStore) Server {
 	server := &Server{store: store}
 	router := gin.Default()
+	router.Use(CORSConfig())
 
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
